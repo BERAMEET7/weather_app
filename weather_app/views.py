28a293adc0dae084b7649878061ad2fe37c32ef7
bera meet <berameet007@gmail.com>
2023-12-29 18:55:38 +0530

    api key removed

--- weather_app/views.py
+++ weather_app/views.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 
 def index(request):
-    api_key = 'f50f3a46b7103803a856219511baaf7f'
+    api_key = 'Enter your api key here'
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
